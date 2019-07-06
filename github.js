8eb0797fe0fd34ef22eff8aeece7e0805cc65c65
rstutalo <richardtutalo@gmail.com>
2019-07-06 00:01:19 -0600

    refactored code in github and ui js files to fix some type reference errors in the browser console with alerts and repos. now working as expected

--- github.js
+++ github.js
@@ -3,26 +3,25 @@ class Github {
     constructor() { // obfuscating api keys since there is no server side code for this application
         this.client_id = 'var _0x8ad1=["\x62\x32\x36\x37\x62\x38\x32\x38\x63\x66\x32\x65\x30\x36\x61\x39\x62\x65\x39\x61"];_0x8ad1[0]';
         this.client_secret ='var _0xf4ca=["\x38\x61\x64\x38\x33\x30\x36\x62\x31\x34\x32\x63\x34\x65\x39\x62\x35\x36\x39\x65\x35\x31\x32\x33\x30\x66\x33\x64\x66\x61\x65\x34\x64\x35\x33\x62\x30\x36\x37\x38"];_0xf4ca[0]';
-        this.repo_count = 10;
-        this.repo_sort = 'created: asc'
+        this.repos_count = 10;
+        this.repos_sort = 'created: asc';
     }
 
     //async get user request
     async getUser(user) {
-        // get user profile
+        // get call to github to fetch the username typed in
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-
-        // get list of latest repositories
-        const repoResponse = await fetch(`https://api.github.com/users/${user}?repos?per_page={this.repo_count}&sort=${this.repo_count}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
-
-        // await response of profileResponse
+        // get call to github to fetch list of their latest repositories
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        // profile set to profileResponse and convert to JSON object
         const profile = await profileResponse.json();
 
+        // repos set to repoResponse and convert to JSON object
         const repos = await repoResponse.json();
 
-
         return {
-            profile
+            profile, 
+            repos
         }
     }
 }
--- ui.js
+++ ui.js
@@ -1,95 +1,96 @@
 class UI {
     constructor() {
-        this.profile = document.getElementById('profile');
+        // make profile in constructor the variable we can reference to display later on
+        this.profile = document.getElementById('profile')
     }
 
+    // display profile in the UI
     showProfile(user) {
-        //console.log(user) - TEST -- working
         this.profile.innerHTML = `
-        <div class="card card-body mb-3">
-            <div class="row">
-                <div class="col-md-3">
-                    <img class="img-fluid mb-2" src="${user.avatar_url}">
-                    <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-6">View User's Profile</a>
-                </div> <!-- end of col -->
-                <div class="col-md-3">
-                    <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-                    <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
-                    <span class="badge badge-success">Followers: ${user.followers}</span>
-                    <span class="badge badge-info">Following: ${user.following}</span>
-                    <br><br>
+            <div class="card card-body mb-3">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img class="img-fluid mb-2" src="${user.avatar_url}">
+                        <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4"></a>
+                    </div> <!-- end of col-3 -->
+                    <div class="col-md-9">
+                        <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
+                        <span class="badge badge-primary">Public Gists: ${user.public_gists}</span>
+                        <span class="badge badge-primary">Followers: ${user.followers}</span>
+                        <span class="badge badge-primary">Following: ${user.following}</span>
+                    </div> <!-- end of col-9 -->
                     <ul class="list-group">
                         <li class="list-group-item">Company: ${user.company}</li>
                         <li class="list-group-item">Website: ${user.blog}</li>
                         <li class="list-group-item">Current Location: ${user.location}</li>
-                        <li class="list-group-item"> Member Since: ${user.created_at}</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <h3 class="page-heading mb-3">Most Recent Repos</h3>
-        <div id="repos"></div>
+                        <li class="list-group-item">Member Since: ${user.created_at}</li>
+                    </ul> <!-- end of ul -->
+                </div> <!-- end of row -->
+            </div> <!-- end of repo card -->
+            <h3>Latest Repositories</h3>
+            <div id="repos"></div>
         `;
     }
 
-    // show latest 10 repos
-    showRepos(repos) {
-        let output = '';
+   // Show user repos
+  showRepos(repos) {
+    let output = '';
 
-        repos.forEach(function(repo) {
-            output += `
-                <div class="card card-body mb-2">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <a href="$${repo.html_url}" target="_blank">${repo.name}</a>
-                        </div>
-                        <div class="col-md-6">
-                            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
-                            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
-                            <span class="badge badge-success">Forks: ${repo.forms_count}</span>
-                        </div>
-                    </div>
-                </div>
-            `;
-        }) // END OF OUTPUT LOOP
+    repos.forEach(function(repo) {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+    });
 
-        // INSERT OUTPUT TO BROWSER
-        document.getElementById('repos').innerHTML = output;
+    // Output repos
+    document.getElementById('repos').innerHTML = output;
+  }
+
+  // Show alert message
+  showAlert(message, className) {
+    // Clear any remaining alerts
+    this.clearAlert();
+    // Create div
+    const div  =  document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container =  document.querySelector('.search-container');
+    // Get search box
+    const search = document.querySelector('.search');
+    // Insert alert
+    container.insertBefore(div, search);
+
+    // Timeout after 3 sec
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  // Clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+
+    if(currentAlert){
+      currentAlert.remove();
     }
+  }
 
-    // displaying alert
-    showAlert(message, className) {
-        // clear any existing alerts
-        this.clearAlert();
-        // create div to store the alert
-        const div = document.createElement('div');
-        div.className = className // second className is the parameter being passed into the showAlert function
-        // adding text to the div
-        div.appendChild(document.createTextNode(message))
-        // get parent for event delegation of inserting the div alert
-        const container = document.querySelector('.search-container');
-        // get search box
-        const search = document.querySelector('.search');
-        // insert alert
-        container.insertBefore(div, search);
-
-        // timeout alert after 3 seconds
-        setTimeout(() => {
-            this.clearAlert()
-        }, 3000)
-    }
-
-    // Clear alert message
-    clearAlert() {
-        const currentAlert = document.querySelector('.alert');
-
-        // check to see if alert already exists
-        if(currentAlert) {
-            currentAlert.remove();
-        }
-    }
-
-    clearProfile() {
-        this.profile.innerHTML = '';
-    }
+  // Clear profile
+  clearProfile() {
+    this.profile.innerHTML = '';
+  }
 }
